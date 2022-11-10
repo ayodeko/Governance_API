c448a7fc1c5880b22204e2b3af21bfc23034a4d8
Ayooluwa Akindeko <aakindeko@appzonegroup.com>
2022-11-10 20:31:50 +0100

    add attendingUser mapping

--- GovernancePortal.Service/Mappings/Maps/MeetingMaps.cs
+++ GovernancePortal.Service/Mappings/Maps/MeetingMaps.cs
@@ -18,6 +18,7 @@ public class MeetingsAutoMapper : Profile
         CreateMap<Meeting, UpdateMeetingAttendingUserGET>();
         CreateMap<Meeting, UpdateMeetingAgendaItemGET>();
         CreateMap<MeetingAgendaItem, AgendaItemPOST>();
+        CreateMap<AttendingUser, AttendingUserPOST>();
         //CreateMap<MeetingModel, MeetingListGET>().ForMember(x => x.AttendanceId, option => option.MapFrom(y => y.Attendance.Id));
     }
 }
