ffd5f9089223e1bcd60b63950d899e30c0548a5d
Ayooluwa Akindeko <akindekoayooluwa@gmail.com>
2022-12-06 17:26:54 +0100

    add extra migrations for resolution

---
+++ GovernancePortal.EF/Migrations/20221205161250_include_resolution_models_in_dbSet.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GovernancePortal.EF.Migrations
+{
+    public partial class include_resolution_models_in_dbSet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Polls",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    isUnlimitedSelection = table.Column<bool>(type: "bit", nullable: false),
+                    MaximumSelection = table.Column<int>(type: "int", nullable: false),
+                    CompanyId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ModelStatus = table.Column<int>(type: "int", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateModified = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Polls", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Votings",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Summary = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsAnonymous = table.Column<bool>(type: "bit", nullable: false),
+                    IsVotingEnded = table.Column<bool>(type: "bit", nullable: false),
+                    CompanyId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ModelStatus = table.Column<int>(type: "int", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateModified = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Votings", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PollItem",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    PollId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PollItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PollItem_Polls_PollId",
+                        column: x => x.PollId,
+                        principalTable: "Polls",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PollUser",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PollId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PollUser", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PollUser_Polls_PollId",
+                        column: x => x.PollId,
+                        principalTable: "Polls",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VotingUser",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    VotingId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Stance = table.Column<int>(type: "int", nullable: false),
+                    StanceReason = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CompanyId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ModelStatus = table.Column<int>(type: "int", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateModified = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VotingUser", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VotingUser_Votings_VotingId",
+                        column: x => x.VotingId,
+                        principalTable: "Votings",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PollItemVote",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    PollItemId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PollUserId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PollItemVote", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PollItemVote_PollUser_PollUserId",
+                        column: x => x.PollUserId,
+                        principalTable: "PollUser",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PollItem_PollId",
+                table: "PollItem",
+                column: "PollId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PollItemVote_PollUserId",
+                table: "PollItemVote",
+                column: "PollUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PollUser_PollId",
+                table: "PollUser",
+                column: "PollId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VotingUser_VotingId",
+                table: "VotingUser",
+                column: "VotingId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PollItem");
+
+            migrationBuilder.DropTable(
+                name: "PollItemVote");
+
+            migrationBuilder.DropTable(
+                name: "VotingUser");
+
+            migrationBuilder.DropTable(
+                name: "PollUser");
+
+            migrationBuilder.DropTable(
+                name: "Votings");
+
+            migrationBuilder.DropTable(
+                name: "Polls");
+        }
+    }
+}
